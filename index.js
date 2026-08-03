a23924172191ac230808eeee814f538768d7739a
urban16899-hash <urban16899@gmail.com>
2026-08-03 23:32:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const TelegramBot = require("node-telegram-bot-api");
 
+const token = "8883131698:AAEdftKxOyFl9xeN7IK9tHfhPmu-umdn2GA";
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  await bot.sendMessage(chatId, "🎉 Welcome!\nAapka welcome hai.");
+
+  // Agle step me video, APK aur channel check add karenge.
+});
